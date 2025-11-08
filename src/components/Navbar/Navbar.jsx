ca2ae3498357d806775aa47d28e055f86eeac6f9
Vegard Gonsholt <vegard.gonsholt@gmail.com>
2025-11-08 11:55:14 +0100

    Remove menu button image from Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,16 +12,6 @@ export const Navbar = () => {
         Portfolio Site
       </a>
       <div className={styles.menu}>
-        <img
-          className={styles.menuBtn}
-          src={
-            menuOpen
-              ? getImageUrl("nav/closeIcon.png")
-              : getImageUrl("nav/menuIcon.png")
-          }
-          alt="menu-button"
-          onClick={() => setMenuOpen(!menuOpen)}
-        />
         <ul
           className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
           onClick={() => setMenuOpen(false)}
